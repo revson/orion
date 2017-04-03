b871ab0be1901838658096ab6fb057fc77134cc7
Revson <revson@gmail.com>
2017-04-03 17:41:15 -0300

    ainda refatorando inimigos

---
+++ Assets/enemies/Hiperion/scripts/EnemyHyperion.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHyperion : MonoBehaviour {
+
+	private int enemyValue;
+	private int score;
+	// Use this for initialization
+	void Start () {
+		
+		enemyValue = PlayerPrefs.GetInt(name+"Value");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (PlayerPrefs.GetFloat("HyperionHp"));
+
+		if (PlayerPrefs.GetFloat ("HyperionHp") == 0) {
+			Finalize();
+		}
+
+
+	}
+
+	void Finalize(){
+		score = PlayerPrefs.GetInt ("score");
+		score += enemyValue;
+		PlayerPrefs.SetInt ("score", score );
+		//hp recebe 1 para sair do loop
+		PlayerPrefs.SetFloat ("HyperionHp", 1);
+	}
+}
---
+++ Assets/enemies/Hiperion/scripts/HiperionGun.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HiperionGun : MonoBehaviour {
+
+	private bool visible;
+
+	public GameObject explosion, energyBall, projectilePrefab;
+
+	private int enemyValue;
+	private float hp;
+	private int score;
+	//private string name;
+
+
+	// Use this for initialization
+	void Start () {
+
+		//name = "Hyperion";
+		hp = 20;
+		//enemyValue = PlayerPrefs.GetInt(name+"Value");
+		StartCoroutine(shooting());
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+
+		switch (col.gameObject.tag){
+		case "Player":			
+			death (true);
+			break;
+
+		case "shieldBall":
+			Destroy (this.gameObject);
+			break;
+
+
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		switch (col.gameObject.tag) {
+		case "simpleShootPlayer":
+			//simple shoot = 1
+			shotHit (1);
+			Destroy (col.gameObject);
+			break;
+
+		case "simpleLaser":
+			//Laser Force = 10
+			shotHit (5);
+
+			break;
+		}
+
+	}
+
+	void shotHit(int hit){		
+		hp -= hit;
+
+		if(hp <= 0 ){
+			hp = 0;
+			//finalize enemy, manda false para nao instanciar a energia.
+			death(false);
+
+		}
+
+	}
+
+	void death (bool col){
+		// add point
+
+		//score = PlayerPrefs.GetInt ("score");
+		//score += enemyValue;
+		//PlayerPrefs.SetInt ("score", score );
+
+		hp = PlayerPrefs.GetFloat("HyperionHp");
+		hp -= 20;
+		PlayerPrefs.SetFloat("HyperionHp", hp);
+		//start explosion
+		Instantiate(explosion, transform.position, transform.rotation);
+
+		// se a morte não foi por colisao, libera energia
+		if (!col) {
+			//start energy
+			Instantiate (energyBall, transform.position, transform.rotation);
+		}
+
+
+		//destroy
+		Destroy(this.gameObject);
+
+	}
+
+	public IEnumerator shooting(){
+
+		yield return new WaitForSeconds (2f);
+		if (visible) {
+			shoot ();
+		}
+
+		StartCoroutine(shooting());
+	}
+
+	void shoot(){
+		Instantiate (projectilePrefab, transform.position, transform.rotation) ;
+	}
+
+	void  OnBecameVisible()
+	{
+		visible = true;
+	}
+	void  OnBecameInvisible()
+	{
+		visible = false;
+	}
+
+
+}
