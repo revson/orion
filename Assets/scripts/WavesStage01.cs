5c18e857d0ebbefa40d0e5061fb4db5d4e338018
Revson <revson@gmail.com>
2017-04-10 17:48:46 -0300

    inicio da wave

--- Assets/scripts/WavesStage01.cs
+++ Assets/scripts/WavesStage01.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class WavesStage01 : MonoBehaviour {
 
+	public GameObject[] enemies;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +13,25 @@ public class WavesStage01 : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public IEnumerator waves(){
+	
+	}
+
+	//Hunter enemy 0
+	public IEnumerator waveHunter(){
+		for (int i = 0; i <= 99; i++) {
+			Vector3 position = transform.position;
+			GameObject preFab;
+			float x = Random.Range (-12.79f, 12.79f);
+			position.x = x;
+			preFab = Instantiate (enemies [0]) as GameObject;
+			preFab.name = "Hunter";
+			preFab.transform.position = position;
+
+			yield return new WaitForSeconds (2f);
+		}
+
 	}
 }
